fd87081a3813cd7947d9ea70dabfbc7fb5cdbc0f
Rahul Kumar <rahulkumar@berkeley.edu>
2023-11-19 11:39:43 -0800

    add pattern and executor libraries

---
+++ utils/srambist/src/executor.rs
@@ -0,0 +1,81 @@
+use crate::pattern::{FixedPattern, FixedSramOp, SramAddr, SramSize, SramWord};
+use crate::state::SramState;
+
+pub trait Executor {
+    fn init(&mut self);
+    fn read(&mut self, addr: SramAddr) -> SramWord;
+    fn write(&mut self, addr: SramAddr, data: SramWord, mask: SramWord);
+
+    fn finish(&mut self);
+}
+
+pub struct IdealExecutor {
+    state: SramState,
+}
+
+impl IdealExecutor {
+    pub fn new(size: SramSize) -> Self {
+        let state = SramState::new(size);
+        Self { state }
+    }
+}
+
+impl Executor for IdealExecutor {
+    fn init(&mut self) {}
+
+    fn read(&mut self, addr: SramAddr) -> SramWord {
+        self.state
+            .read(addr)
+            .expect("tried to read from an uninitialized address")
+    }
+
+    fn write(&mut self, addr: SramAddr, data: SramWord, mask: SramWord) {
+        self.state.write(addr, data, mask);
+    }
+
+    fn finish(&mut self) {}
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+pub struct BistError {
+    op: usize,
+    expected: SramWord,
+    received: SramWord,
+}
+pub fn execute<E: Executor>(pattern: FixedPattern, mut ex: E) -> Result<(), BistError> {
+    println!("Beginning SRAM BIST test");
+
+    ex.init();
+    let res = execute_inner(pattern, &mut ex);
+    ex.finish();
+
+    res
+}
+
+fn execute_inner<E: Executor>(pattern: FixedPattern, ex: &mut E) -> Result<(), BistError> {
+    for (i, op) in pattern.ops().enumerate() {
+        match op {
+            FixedSramOp::Read { data, addr } => {
+                print!("Reading {addr:#x}...\t");
+                let dout = ex.read(addr);
+                if dout == data {
+                    println!("OK");
+                } else {
+                    println!("ERROR: got {dout:#x}, expected {data:#x}");
+                    return Err(BistError {
+                        op: i,
+                        expected: data,
+                        received: dout,
+                    });
+                }
+            }
+            FixedSramOp::Write { data, addr, mask } => {
+                print!("Writing {addr:#x} with data = {data:#x}, mask = {mask:#x}...\t");
+                ex.write(addr, data, mask);
+                println!("DONE");
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ utils/srambist/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod executor;
+pub mod pattern;
+pub mod state;
+
+#[cfg(test)]
+mod tests;
