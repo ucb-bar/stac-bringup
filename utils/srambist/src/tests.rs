79d6a23f3cd8cf33ebd8f0f29904e9c2470b9402
Rahul Kumar <rahulkumar@berkeley.edu>
2023-12-08 13:22:26 -0800

    feat: add srambist bebe executor (#2)
    * feat: add srambist bebe executor
    
    * fix args name collision
    
    * add bebe scratchpad executor

--- utils/srambist/src/tests.rs
+++ utils/srambist/src/tests.rs
@@ -1,3 +1,4 @@
+use crate::bebe::BebeScratchpadExecutor;
 use crate::executor::{execute, IdealExecutor};
 use crate::pattern::{FixedPattern, Pattern, SramSize};
 
@@ -7,4 +8,12 @@ fn mats_plus_ideal_executor() {
     let ex = IdealExecutor::new(size);
     let pat = FixedPattern::new(Pattern::mats_plus(), size, 1);
     execute(pat, ex).expect("MATS+ pattern should execute correctly with an ideal executor");
+}
+
+#[test]
+fn mats_plus_bebe_scratchpad() {
+    let size = SramSize::new(64, 512, 8);
+    let ex = BebeScratchpadExecutor;
+    let pat = FixedPattern::new(Pattern::mats_plus(), size, 1);
+    execute(pat, ex).expect("failed to run MATS+ pattern");
 }
